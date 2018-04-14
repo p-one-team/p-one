37fb9b22c3b486f432da6d14b48ff70433496ef8
rover5056 <505659051@qq.com>
2018-04-14 11:27:50 +0800

    fix bugs3

--- src/view/home/home-component.js
+++ src/view/home/home-component.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
 import style from './home.less'
 
-import { InputItem, Button, WhiteSpace, Flex, Checkbox } from 'antd-mobile';
+import { InputItem, Button, WhiteSpace } from 'antd-mobile';
 import { createForm } from 'rc-form';
 
 
@@ -20,30 +20,6 @@ class HomeComponent extends Component {
 
     }
 
-    loginBox = (getFieldProps) => (
-        <div styleName="loginBox">
-            <InputItem
-                {...getFieldProps('phoneNumber')}
-                type="phone"
-                placeholder="1xx xxxx xxxx"
-                clear
-            >手机号码</InputItem>
-            <InputItem
-                {...getFieldProps('verificationCode')}
-                maxLength={4}
-                type={'digit'}
-                clear
-            >验证码</InputItem>
-            <InputItem
-                {...getFieldProps('password')}
-                type={'password'}
-                clear
-            >密码</InputItem>
-
-            <WhiteSpace />
-            <Button type="primary" inline size="large" onClick={this.submit}>登录</Button>
-        </div>
-    );
 
 
     componentDidMount() {
@@ -55,8 +31,8 @@ class HomeComponent extends Component {
             console.log(error, value);
         });
     }
-    goSign() {
-        this.props.history.push('/miao')
+    goSign = () => {
+        this.props.history.push('/signUp')
     }
     handleClick() {
         this.customFocusInst.focus();
@@ -67,11 +43,9 @@ class HomeComponent extends Component {
         // let errors;
         // const { getFieldProps, getFieldError } = this.props.form;
         console.log(this.props)
-        const AgreeItem = Checkbox.AgreeItem;
         return (
             <div styleName="wrap">
                 <p styleName="title">GT</p>
-                <loginBox />
                 <div styleName="loginBox">
                     <InputItem
                         {...getFieldProps('phoneNumber')}
@@ -93,42 +67,10 @@ class HomeComponent extends Component {
 
                     <WhiteSpace />
                     <Button type="primary" inline size="large" onClick={this.submit}>登录</Button>
+                    <Button type="primary" inline size="large" onClick={this.goSign}>注册</Button>
                 </div>
 
-                <div styleName="signUp">
-                    <InputItem
-                        {...getFieldProps('phoneNumber')}
-                        type="phone"
-                        placeholder="1xx xxxx xxxx"
-                        clear
-                    >手机号码</InputItem>
-                    <InputItem
-                        {...getFieldProps('password')}
-                        type={'password'}
-                        clear
-                    >密码</InputItem>
-                    <InputItem
-                        {...getFieldProps('password')}
-                        type={'password'}
-                        clear
-                    >密码</InputItem>
-                    <InputItem
-                        {...getFieldProps('verificationCode')}
-                        maxLength={4}
-                        type={'digit'}
-                        clear
-                    >验证码</InputItem>
-                    <WhiteSpace />
-                    <Button type="primary" inline size="large" onClick={this.submit}>登录</Button>
-                    <Button type="primary" inline size="large" onClick={this.goSign}>注册</Button>
-                    <Flex>
-                        <Flex.Item>
-                            <AgreeItem data-seed="logId" onChange={e => console.log('checkbox', e)}>
-                                Agree <a onClick={(e) => { e.preventDefault(); alert('agree it'); }}>agreement</a>
-                            </AgreeItem>
-                        </Flex.Item>
-                    </Flex>
-                </div>
+
 
             </div>
         )
