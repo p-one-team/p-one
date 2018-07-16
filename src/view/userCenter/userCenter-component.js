8b5d5133fc78d54fe747a835fe5fbc91ef07d246
rover5056 <505659051@qq.com>
2018-07-16 15:52:23 +0800

    签到

--- src/view/userCenter/userCenter-component.js
+++ src/view/userCenter/userCenter-component.js
@@ -14,8 +14,8 @@ class UserCenterComponent extends Component {
 		}
 	}
 
-	componentWillMount(){
-		this.props.refreshUserInfo('',()=>{
+	componentWillMount() {
+		this.props.refreshUserInfo('', () => {
 			this.setState({
 				isSign: this.props.userInfos.IsCheckIn
 			})
@@ -23,7 +23,7 @@ class UserCenterComponent extends Component {
 	}
 
 	updateUserInfo = () => {
-		this.props.refreshUserInfo('',()=>{
+		this.props.refreshUserInfo('', () => {
 			this.setState({
 				isSign: this.props.userInfos.IsCheckIn
 			})
@@ -31,14 +31,14 @@ class UserCenterComponent extends Component {
 	}
 
 	sign = () => {
-		this.props.signIn('',()=>{
+		this.props.signIn('', () => {
 			this.updateUserInfo()
 		})
 	}
 
 	render() {
 		let userInfos = this.props.userInfos;
-		
+
 		return (
 			<div styleName="wrap">
 				<div styleName="header">
@@ -46,9 +46,13 @@ class UserCenterComponent extends Component {
 						<img styleName="user-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524227131869&di=dcca65d33e7ce856a261f31c847ddf90&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%253D580%2Fsign%3D7d86311748ed2e73fce98624b703a16d%2Fcb514fc2d5628535ae210cb292ef76c6a6ef6365.jpg" alt="" />
 						<div styleName="user-info">{userInfos.NickName}</div>
 					</div>
-					{this.state.isSign ? <div styleName="hasSigned">已签到</div> : <div styleName="sign" onClick={()=>this.sign()}>点击签到</div>}
+					{/* {this.state.isSign ? <div styleName="hasSigned">已签到</div> : <div styleName="sign" onClick={() => this.sign()}>点击签到</div>} */}
 				</div>
 				<ul>
+					<li>
+						<div> <span className="iconfont icon-favorfill"></span>签到送T豆</div>
+						{this.state.isSign ? <div styleName="hasSigned">已签到</div> : <div styleName="sign" onClick={() => this.sign()}>点击签到</div>}
+					</li>
 					<li onClick={() => this.props.goInventory()}>
 						<div> <span className="iconfont icon-goodsfill"></span>我的库存</div>
 						<div className="iconfont icon-previewright"></div>
@@ -65,18 +69,6 @@ class UserCenterComponent extends Component {
 						<div> <span className="iconfont icon-writefill"></span>意见反馈</div>
 						<div className="iconfont icon-previewright"></div>
 					</li>
-					{/* <li onClick={() => this.props.goMessage()}>
-						<div> <span className="iconfont icon-messagefill"></span>我的消息</div>
-						<div className="iconfont icon-previewright"></div>
-					</li>
-					<li>
-						<div> <span className="iconfont icon-gift1"></span>我的礼物</div>
-						<div className="iconfont icon-previewright"></div>
-					</li>
-					<li>
-						<div> <span className="iconfont icon-favorfill"></span>我的关注</div>
-						<div className="iconfont icon-previewright"></div>
-					</li> */}
 					<li onClick={() => this.props.goHelpSuggest()}>
 						<div> <span className="iconfont icon-help1"></span>帮助和建议</div>
 						<div className="iconfont icon-previewright"></div>
