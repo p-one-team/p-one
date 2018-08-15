40122e7567f779eddebcbde28b1029223eb4552e
rongkang <rongkang@baidu.com>
2018-08-15 19:02:46 +0800

    修改绑定流程

--- src/view/match/match-component.js
+++ src/view/match/match-component.js
@@ -107,7 +107,13 @@ class MatchComponent extends Component {
 
     goBind = () => {
         this.closeAlert()
-        window.location.href = `${steamLogin}?userId=${this.props.userInfos.UserId}&mobile=${this.props.userInfos.Mobile}`
+        let ref = window.cordova.InAppBrowser.open(`${steamLogin}?userId=${this.props.userInfos.UserId}&mobile=${this.props.userInfos.Mobile}`, '_blank', 'location=no')
+        //  cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+        let InAppBrowserCallback = ()=>{
+            alert('更新userInfo');
+        }
+        ref.addEventListener('exit', InAppBrowserCallback);
+
         //去绑定Steam
     }
 
